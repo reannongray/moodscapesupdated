1beccc85a8ca664d14b3a8f56dcf39bd0a57f8d8
Reannon Gray <168271727+reannongray@users.noreply.github.com>
2024-12-13 00:31:38 -0500

    viteupdate

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/moodscapesupdated/', // Base URL for GitHub Pages
   server: {
     port: 3000,
     open: true
   },
-  root: './src',
   build: {
     outDir: '../dist',
     emptyOutDir: true
@@ -17,4 +15,4 @@ export default defineConfig({
       '@': resolve(__dirname, './src')
     }
   }
-});
+})
